cc92190d69627d8027e32c717318f67f4975e849
Kaio Rosa <kaio.rosa@motora.ai>
2020-04-17 22:26:14 -0300

    add about component + redirect about route

--- kaio-rosa-website/src/app/app.module.ts
+++ kaio-rosa-website/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
